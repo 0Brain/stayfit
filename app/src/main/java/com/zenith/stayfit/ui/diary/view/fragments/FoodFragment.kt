463457157a8e9d0349297de9a3854bb5e6b125ee
KKApaya <apaya1606@gmail.com>
2020-08-27 03:26:28 +0530

    Diary Fragments

---
+++ app/src/main/java/com/zenith/stayfit/ui/diary/view/fragments/FoodFragment.kt
@@ -0,0 +1,17 @@
+package com.zenith.stayfit.ui.diary.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.zenith.stayfit.R
+
+
+class FoodFragment : Fragment(R.layout.fragment_add_food) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/zenith/stayfit/ui/diary/view/fragments/WaterFragment.kt
@@ -0,0 +1,15 @@
+package com.zenith.stayfit.ui.diary.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.zenith.stayfit.R
+
+
+class WaterFragment : Fragment(R.layout.fragment_add_water) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
